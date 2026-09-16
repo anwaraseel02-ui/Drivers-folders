c7497a16094bdc0f484c0ed5ed54526e87d6b943
Aseel Anwar <anwaraseel02@gmail.com>
2026-09-16 17:39:34 +0300

    Fix LIB folder and add STD_Types.h

---
+++ LIB/STD_Types.h
@@ -0,0 +1,17 @@
+#pragma once 
+
+// all unsigned starts with "u"
+// all signed starts with "s"
+// the number next to each one is the size in bits
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+typedef unsigned long long int u64;
+
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed long int s32;
+typedef signed long long int s64;
+
+typedef float f32;
+typedef double d64; 
