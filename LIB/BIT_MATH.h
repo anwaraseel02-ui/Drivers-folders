d7e60e6da04ef2b1c435be874bf6085ce6329323
Aseel Anwar <anwaraseel02@gmail.com>
2026-09-17 14:09:57 +0300

    More definitions on functions

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -4,3 +4,6 @@
 #define TOG_BIT(REG, BIT) REG ^=(1<<BIT)
 #define CLR_BIT(REG, BIT) REG &=~(1<<BIT)
 #define GET_BIT(REG, BIT) ((REG &(1<<BIT))>>BIT)
+
+#define IS_BIT_SET(REG, BIT) ((REG &(1<<BIT))>>BIT)
+#define IS_BIT_CLR(REG, BIT) (!((REG &(1<<BIT))>>BIT))
